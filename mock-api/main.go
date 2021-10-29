3989691ad5349223c8a77de1b4808bb3bc87a188
t0mmy <tommycsth61@gmail.com>
2021-10-29 09:24:32 +0900

    feat: シラバス検索の結果を返すAPIを作成

--- mock-api/main.go
+++ mock-api/main.go
@@ -24,8 +24,19 @@ func getGradeDists(c echo.Context) error {
 	return c.String(http.StatusOK, string(bytes))
 }
 
+func getSyllabusResults(c echo.Context) error {
+	fmt.Println(c.FormValue("period"), c.FormValue("year"), c.FormValue("dayOfWeek"), c.FormValue("periodOfTime"), c.FormValue("degree"), c.FormValue("typeOfCourse"))
+	bytes, err := ioutil.ReadFile("./data/syllabus/syllabus_search_result.json")
+	if err != nil {
+		return c.String(http.StatusNotFound, "No Grade Distribution") // TODO:
+	}
+
+	return c.String(http.StatusOK, string(bytes))
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/grade-distribution/:courseId", getGradeDists)
+	e.GET("/search/syllabus", getSyllabusResults)
 	e.Logger.Fatal(e.Start(":10000"))
 }
