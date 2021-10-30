e01ea1ee3e2339c56b955e8885d65ff27eb8e885
t0mmy <tommycsth61@gmail.com>
2021-10-30 09:26:25 +0900

    feat: 図書館の蔵書状況を取得する際に, useEffectで無限ループが起こるバグを修正

--- frontend/src/organisms/Book.tsx
+++ frontend/src/organisms/Book.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { Skeleton, Badge, Heading, Stack } from '@chakra-ui/react';
+import { produce } from 'immer';
 import { searchISBN, searchLentState } from '../api/book';
 
 type Props = {
@@ -6,25 +8,150 @@ type Props = {
 };
 
 export const Book: React.FC<Props> = ({ bookInfo }) => {
-  const [res, setRes] = useState<any>('');
+  const [res, setRes] = useState<{
+    doshisha: any;
+    osaka_pref: any;
+    osaka_city: any;
+  }>({ doshisha: null, osaka_pref: null, osaka_city: null });
+  const [isFetching, setIsFetching] = useState(false);
+  const [conn, setConn] = useState(1);
+  const [isbn, setIsbn] = useState<any>('');
+  const [fetchError, setFetchError] = useState(false);
   useEffect(() => {
     (async () => {
-      const isbn_obj = await searchISBN(
-        bookInfo?.split('(')[0].replace(/\r?\n/g, ' '),
-      );
-      const isbn = isbn_obj.ISBN_10 || isbn_obj.ISBN_13;
-      if (!isbn) console.log('not found');
-      else {
-        const ress = await searchLentState(isbn);
-        setRes(JSON.stringify(ress));
+      setIsFetching(true);
+      if (isbn === '') {
+        const isbn_obj = await searchISBN(
+          bookInfo?.split('(')[0].replace(/\r?\n/g, ' '),
+        );
+
+        console.log(isbn_obj);
+        setIsbn(isbn_obj.ISBN_10 || isbn_obj.ISBN_13);
       }
+
+      if (!isbn) return;
+
+      if (conn === 1) {
+        let res_t = res;
+
+        /* eslint-disable prefer-destructuring */
+
+        let states;
+        try {
+          /* eslint-disable no-await-in-loop */
+          states = await searchLentState(isbn);
+          console.log(states);
+        } catch {
+          console.log('wwww');
+
+          return;
+        }
+
+        const doshisha = states.books[isbn].Univ_Doshisha;
+        const Osaka_Osaka = states.books[isbn].Osaka_Osaka;
+        const Osaka_Pref = states.books[isbn].Osaka_Pref;
+        /* eslint-disable no-param-reassign */
+        res_t = produce(res, d => {
+          d.doshisha = doshisha;
+          d.osaka_city = Osaka_Osaka;
+          d.osaka_pref = Osaka_Pref;
+        });
+
+        setConn(states.continue);
+
+        setRes(res_t);
+        setIsFetching(false);
+      }
+
+      /**
+       * TODO:
+       * Warning: Can't perform a React state update on an unmounted component.
+       * This is a no-op, but it indicates a memory leak in your application.
+       * To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+       */
     })();
-  });
+  }, [bookInfo, res, isbn, conn]);
 
   return (
-    <div>
-      {bookInfo}
-      <p>{res}</p>
+    <div style={{ marginBottom: '10px' }}>
+      {/* {JSON.stringify(res)} */}
+      <p style={{ marginBottom: '5px', marginTop: '2px' }}>{bookInfo}</p>
+      <Heading as="h3" size="sm">
+        図書館の貸し出し状況
+      </Heading>
+      {isbn !== null && isbn !== '' ? (
+        <>
+          <Heading as="h4" size="sm">
+            Doshisha
+          </Heading>
+          {Object.entries(res?.doshisha?.libkey || {})?.map((arr: any) => {
+            return (
+              <Badge
+                colorScheme={
+                  arr[1] === '貸出可' || arr[1] === '蔵書あり' ? 'green' : 'red'
+                }
+                variant="outline"
+              >
+                <a
+                  href={res?.doshisha?.reserveurl}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ fontSize: '12px' }}
+                >
+                  {arr[0]}: {arr[1]}
+                </a>
+              </Badge>
+            );
+          })}
+          <Heading as="h4" size="sm">
+            Osaka pref
+          </Heading>
+          {Object.entries(res?.osaka_pref?.libkey || {}).map((arr: any) => {
+            return (
+              <Badge
+                variant="outline"
+                colorScheme={
+                  arr[1] === '貸出可' || arr[1] === '蔵書あり' ? 'green' : 'red'
+                }
+              >
+                <a
+                  href={res?.osaka_pref?.reserveurl}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ fontSize: '12px' }}
+                >
+                  {arr[0]}: {arr[1]}
+                </a>
+              </Badge>
+            );
+          })}
+          <Heading as="h4" size="sm">
+            Osaka city
+          </Heading>
+
+          {Object.entries(res?.osaka_city?.libkey || {}).map((arr: any) => {
+            return (
+              <Badge
+                colorScheme={
+                  arr[1] === '貸出可' || arr[1] === '蔵書あり' ? 'green' : 'red'
+                }
+                variant="outline"
+              >
+                <a
+                  href={res?.osaka_city?.reserveurl}
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{ fontSize: '12px' }}
+                >
+                  {arr[0]}: {arr[1]}
+                </a>
+              </Badge>
+            );
+          })}
+        </>
+      ) : (
+        <div>情報を取得できませんでした</div>
+      )}
     </div>
   );
 };
